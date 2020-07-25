67a0ef414c4208d72284d0ea47b597992a4ac0b6
Priyanka <awatramanipriyanka1@gmail.com>
2020-07-25 15:41:33 +0530

    Added Nudge Logo

--- index.php
+++ index.php
@@ -53,14 +53,14 @@ $auth->build(); //it automatically creates a table for me
         <div class="container">
 
             <!-- Outer Row -->
-            <div class="row justify-content-center">
+            <div class="row justify-content-center" style="padding-top:200px;">
 
                 <div class="col-xl-10 col-lg-12 col-md-9">
-                    <div class="card o-hidden border-0 shadow-lg my-5">
+                    <!-- <div class="card o-hidden border-0 shadow-lg my-5"> -->
                         <div class="card-body p-0">
                             <!-- Nested Row within Card Body -->
-                            <div class="row">
-                                <img src="" alt="">
+                            <div class="row" >
+                                <img src="images/nudge-logo.png" alt="" width="800px">
                             </div>
                         </div>
                     </div>
