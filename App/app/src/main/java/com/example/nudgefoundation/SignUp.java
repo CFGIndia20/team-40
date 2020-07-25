596e112e420620b9ed84d732e64a4b6aa944aeff
Mrinal Raj <mrinalraj2809@gmail.com>
2020-07-25 19:34:29 +0530

    Sign up Part started

--- App/app/src/main/java/com/example/nudgefoundation/SignUp.java
+++ App/app/src/main/java/com/example/nudgefoundation/SignUp.java
@@ -3,12 +3,30 @@ package com.example.nudgefoundation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SignUp extends AppCompatActivity {
-
+public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    Spinner spinnerUserType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        spinnerUserType             = findViewById(R.id.spinnerUserType);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.user_type,android.R.layout.simple_spinner_dropdown_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerUserType.setAdapter(arrayAdapter);
+        spinnerUserType.setOnItemSelectedListener(this);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent,View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
