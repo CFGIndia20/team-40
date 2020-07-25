c3a5d795d84cc7875573f651552d17e5648d9d02
Mrinal Raj <mrinalraj2809@gmail.com>
2020-07-25 21:45:07 +0530

    Student login Fully functional

--- App/app/src/main/java/com/example/nudgefoundation/MemberTeacher.java
+++ App/app/src/main/java/com/example/nudgefoundation/MemberTeacher.java
@@ -1,4 +1,15 @@
 package com.example.nudgefoundation;
 
 public class MemberTeacher {
+    String teacher_name;
+    String teacher_id;
+    String student_email;
+    String student_phone;
+    String teacher_password;
+    String user_type;
+    String user_id;
+
+    public MemberTeacher() {
+    }
+
 }
